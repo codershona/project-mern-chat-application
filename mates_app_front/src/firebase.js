7a832d757b89d853b36154f511e91c53484d111d
codershona <falgunimst95@gmail.com>
2021-05-20 14:05:58 +0200

    COnfiguring firebase and creating the sidebar component for mern stack matesapp app

---
+++ mates_app_front/src/firebase.js
@@ -0,0 +1,17 @@
+// configuring in firebase
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAk2xWK5sC26XdcsbNVeiTGH-uMdSimZNE",
+    authDomain: "mates-app-f9323.firebaseapp.com",
+    projectId: "mates-app-f9323",
+    storageBucket: "mates-app-f9323.appspot.com",
+    messagingSenderId: "1034645102233",
+    appId: "1:1034645102233:web:e471b3a3ed3f35f07c2208"
+  };
+
+
+
+
+
+
+
