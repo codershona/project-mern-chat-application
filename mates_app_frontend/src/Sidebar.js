0e7b45c41bb63837d3af4494de6ab8a1c0ce2a7e
codershona <falgunimst95@gmail.com>
2021-05-22 14:34:04 +0200

    working in sidebar chat info headline adn make it margin left

--- mates_app_frontend/src/Sidebar.js
+++ mates_app_frontend/src/Sidebar.js
@@ -36,6 +36,9 @@ function Sidebar() {
                <SidebarChats />
                <SidebarChats />
                <SidebarChats />
+               <SidebarChats />
+               <SidebarChats />
+               <SidebarChats />
            </div>
         </div>
     );
