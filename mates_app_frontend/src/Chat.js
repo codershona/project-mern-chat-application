20577256510b5b6d3c9f038ae880ae4b5a4333de
codershona <falgunimst95@gmail.com>
2021-05-22 16:04:16 +0200

    Chat room header styling done and we had use flex to styled the components

--- mates_app_frontend/src/Chat.js
+++ mates_app_frontend/src/Chat.js
@@ -1,14 +1,30 @@
 import React from "react";
 import "./Chat.css";
-import { Avatar } from '@material-ui/core';
+import { Avatar, IconButton } from '@material-ui/core';
+import YoutubeSearchedForIcon from '@material-ui/icons/YoutubeSearchedFor';
+import AttachmentIcon from '@material-ui/icons/Attachment';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
 
 function Chat() {
     return (
         <div className="chat">
             <div className="chat_Headers">
             <Avatar />
-            <div className="chat_HeaderInfo">
-                
+            <div className="chat_HeadersInfo">
+                <h3>Contact Name</h3>
+                <p> tap here for contact info.... </p>
+            </div>
+            <div className="chat_HeadersRight">
+                <IconButton>
+                   <YoutubeSearchedForIcon />
+                </IconButton>
+                <IconButton>
+                  <ExpandMoreIcon />
+                </IconButton>
+                <IconButton>
+                  <AttachmentIcon />
+                </IconButton>
             </div>
             </div>
         </div>
